616f3a82865e67016192aa5f0a6f7761d4a2382f
Cat <catarremann@gmail.com>
2020-02-19 14:36:59 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Spam_Checker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string blackListWord = "killer";
+            bool isSpam = false;
+            string message = Console.ReadLine();
+            if (message.Contains(blackListWord))
+            {
+                isSpam = true;
+                Console.WriteLine("The message is spam");
+            }
+            else
+            {
+                isSpam = false;
+                Console.WriteLine("The message is not spam.");
+
+            }
+
+        }
+    }
+}
